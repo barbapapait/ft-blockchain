DNAProject/DNA

Fix double spent issue in transaction pool
Removed duplicated inputs from UTXO Inputs List of transaction pool
when remove double spent transaction, which will cause double spend
when any transactions still refer to the inputs removed before.

Signed-off-by: Ziyan Zhou <zhouziyan@hotmail.com>

net/node/transactionPool.go
@@ -9,6 +9,7 @@ import (
 	"DNA/core/transaction/payload"
 	va "DNA/core/validation"
 	. "DNA/errors"
+	"errors"
 	"fmt"
 	"sync"
 )
@@ -91,11 +92,10 @@ func (this *TXNPool) GetTransaction(hash common.Uint256) *transaction.Transactio
 
 //verify transaction with txnpool
 func (this *TXNPool) verifyTransactionWithTxnPool(txn *transaction.Transaction) bool {
-	//check weather have duplicate UTXO input,if occurs duplicate, just keep the latest txn.
-	ok, duplicateTxn := this.apendToUTXOPool(txn)
-	if !ok && duplicateTxn != nil {
-		log.Info(fmt.Sprintf("txn=%x duplicateTxn UTXO occurs with txn in pool=%x,keep the latest one.", txn.Hash(), duplicateTxn.Hash()))
-		this.removeTransaction(duplicateTxn)
+	// check if the transaction includes double spent UTXO inputs
+	if err := this.verifyDoubleSpend(txn); err != nil {
+		log.Info(err)
+		return false
 	}
 	//check issue transaction weather occur exceed issue range.
 	if ok := this.summaryAssetIssueAmount(txn); !ok {
@@ -130,19 +130,25 @@ func (this *TXNPool) removeTransaction(txn *transaction.Transaction) {
 }
 
 //check and add to utxo list pool
-func (this *TXNPool) apendToUTXOPool(txn *transaction.Transaction) (bool, *transaction.Transaction) {
+func (this *TXNPool) verifyDoubleSpend(txn *transaction.Transaction) error {
 	reference, err := txn.GetReference()
 	if err != nil {
-		return false, nil
+		return err
 	}
-	for k, _ := range reference {
-		t := this.getInputUTXOList(k)
-		if t != nil {
-			return false, t
+	inputs := []*transaction.UTXOTxInput{}
+	for k := range reference {
+		if txn := this.getInputUTXOList(k); txn != nil {
+			return errors.New(fmt.Sprintf("double spent UTXO inputs detected, "+
+				"transaction hash: %x, input: %s, index: %s",
+				txn.Hash(), k.ToString()[:64], k.ToString()[64:]))
 		}
-		this.addInputUTXOList(txn, k)
+		inputs = append(inputs, k)
 	}
-	return true, nil
+	for _, v := range inputs {
+		this.addInputUTXOList(txn, v)
+	}
+
+	return nil
 }
 
 //clean txnpool utxo map

barbapapait/ft-blockchain

net/node/transactionPool.go
@@ -9,6 +9,7 @@ import (
 	"DNA/core/transaction/payload"
 	va "DNA/core/validation"
 	. "DNA/errors"
+	"errors"
 	"fmt"
 	"sync"
 )
@@ -91,11 +92,10 @@ func (this *TXNPool) GetTransaction(hash common.Uint256) *transaction.Transactio
 
 //verify transaction with txnpool
 func (this *TXNPool) verifyTransactionWithTxnPool(txn *transaction.Transaction) bool {
-	//check weather have duplicate UTXO input,if occurs duplicate, just keep the latest txn.
-	ok, duplicateTxn := this.apendToUTXOPool(txn)
-	if !ok && duplicateTxn != nil {
-		log.Info(fmt.Sprintf("txn=%x duplicateTxn UTXO occurs with txn in pool=%x,keep the latest one.", txn.Hash(), duplicateTxn.Hash()))
-		this.removeTransaction(duplicateTxn)
+	// check if the transaction includes double spent UTXO inputs
+	if err := this.apendToUTXOPool(txn); err != nil {
+		log.Info(err)
+		return false
 	}
 	//check issue transaction weather occur exceed issue range.
 	if ok := this.summaryAssetIssueAmount(txn); !ok {
@@ -130,19 +130,25 @@ func (this *TXNPool) removeTransaction(txn *transaction.Transaction) {
 }
 
 //check and add to utxo list pool
-func (this *TXNPool) apendToUTXOPool(txn *transaction.Transaction) (bool, *transaction.Transaction) {
+func (this *TXNPool) apendToUTXOPool(txn *transaction.Transaction) error {
 	reference, err := txn.GetReference()
 	if err != nil {
-		return false, nil
+		return err
 	}
-	for k, _ := range reference {
-		t := this.getInputUTXOList(k)
-		if t != nil {
-			return false, t
+	inputs := []*transaction.UTXOTxInput{}
+	for k := range reference {
+		if txn := this.getInputUTXOList(k); txn != nil {
+			return errors.New(fmt.Sprintf("double spent UTXO inputs detected, "+
+				"transaction hash: %x, input: %s, index: %s",
+				txn.Hash(), k.ToString()[:64], k.ToString()[64:]))
 		}
-		this.addInputUTXOList(txn, k)
+		inputs = append(inputs, k)
 	}
-	return true, nil
+	for _, v := range inputs {
+		this.addInputUTXOList(txn, v)
+	}
+
+	return nil
 }
 
 //clean txnpool utxo map